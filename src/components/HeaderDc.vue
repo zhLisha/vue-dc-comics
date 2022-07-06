0872876c38fd85bf7eb9775d5f837dd0629f8853
zhLisha <lisazhou1709@gmail.com>
2022-07-06 14:43:34 +0200

    Header with dynamic nav

--- src/components/HeaderDc.vue
+++ src/components/HeaderDc.vue
@@ -1,15 +1,115 @@
 <template>
-    <div class="container">
-        <h1>ciao</h1>
+    <div class="header">
+        <div class="container flex">
+            <!-- Logo -->
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="Dc Logo">
+            </div>
+            <!-- Nav -->
+            <nav>
+                <ul class="flex">
+                    <li v-for="(list, index) in navList" :key="index" :class="{'current': list.current}">
+                        <a href="list.url"> {{ list.text }} </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'HeaderDc'
+    name: 'HeaderDc',
+    data() {
+        return {
+            navList: [
+                {
+                    text: 'CHARACTERS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'COMICS',
+                    url: '#',
+                    current: true
+                },
+                 {
+                    text: 'MOVIES',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'TV',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'GAMES',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'COLLECTIONS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'VIDEO',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'FANS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'NEWS',
+                    url: '#',
+                    current: false
+                },
+                 {
+                    text: 'SHOP',
+                    url: '#',
+                    current: false
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../common/variables';
 
+.header {
+    height: 80px;
+
+    img {
+        width: 60px;
+        display: inline-block;
+        padding-top: 8px;
+    }
+
+    .container {
+        justify-content: space-between;
+        align-items: center;
+    }
+}
+
+ul {
+    font-size: 11px;
+
+    li {
+        padding:0 10px;
+        
+        &.current:after {
+        content: "";
+        border-bottom: 4px solid $mainColor;
+        color: blue;
+        margin: 0 auto;
+        display: block;
+        }
+    }
+}
 </style>
