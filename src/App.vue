8e8e5c3ebc008b61906897bc983f46c766232242
zhLisha <lisazhou1709@gmail.com>
2022-07-06 13:50:22 +0200

    Added common containers

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-    <header>
-      <h1 class="container">ciao</h1>
-    </header>
+    <div id="app">
+
+      <header>
+        <HeaderDc />
+      </header>
+
+    </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import HeaderDc from './components/HeaderDc.vue'
 
 export default {
-
+  name: "App", 
+  components: {
+    HeaderDc
+  }
 }
 </script>
 
 <style lang="scss">
 @import './common/variables';
 @import './common/common';
+
+
 </style>
---
+++ src/components/HeaderDc.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1>ciao</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderDc'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
